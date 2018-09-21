882de814fdf1086927a64fb197b73ccaa0344caf
kevinbai0 <kevin.c.bai0@gmail.com>
2018-09-21 00:03:46 -0400

    Added redirects to certain url requests

--- index.js
+++ index.js
@@ -21,18 +21,19 @@ var transporter = nodemailer.createTransport({
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	// Handle React routing, return all requests to React app
 	app.get('/', function(req, res) {
+		res.redirect("https://www.kevinbai.design");
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 	app.get('/contact', function(req, res) {
+		res.redirect("https://www.kevinbai.design/contact");
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 	app.get('/work', function(req, res) {
+		res.redirect("https://www.kevinbai.design/work");
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 	app.get('/about', function(req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-	});
-	app.get('/blog', function(req, res) {
+		res.redirect("https://www.kevinbai.design/about");
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 //}
