c79a2b093329667da154039eb129f06d6f1d0ec2
kevinbai0 <kevin.c.bai0@gmail.com>
2018-11-08 18:04:03 -0500

    regu.ar expression parses links and searches for them

--- client/src/components/pages/SearchEngine.js
+++ client/src/components/pages/SearchEngine.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import search from "../../methods/search";
 
 class SearchEngine extends Component {
     constructor(props) {
         super(props);
         this.state = {
             imageURL: "",
-            currentInput: ""
+            currentInput: "",
+            autocompleteOptions: [
+
+            ],
+            isLink: false
         }
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -28,7 +33,13 @@ class SearchEngine extends Component {
                     <input type="text" className="search-bar" id="mainSearchBar" placeholder="Search" onChange={(e) => this.handleInputChange(e)} autoFocus/>
                     <input type="submit" value="submit" className="hidden"/>
                 </form>
-                
+                <div className="dropdown-menu">
+                    {
+                        this.state.autocompleteOptions.map((option, i) => {
+                            return <div key={i} className="auto-complete-option">{option.name}</div>
+                        })
+                    }
+                </div>
             </div>
         </div>
     }
@@ -36,14 +47,31 @@ class SearchEngine extends Component {
         return fetch("/api/photoOfTheDay").then((res) => res.json());
     }
     handleSubmit(e) {
-        console.log("Handle submit");
         e.preventDefault();
-        window.location="https://www.google.com/search?source=hp&ei=kMbjW4bEIYnRjwSJtKPIDQ&q=" + this.state.currentInput;
+        if (this.state.isLink) {
+            let hasPrefix = this.state.currentInput.indexOf("http") == 0 
+            console.log(this.state.currentInput);
+            window.location = hasPrefix ? this.state.currentInput : "http://" + this.state.currentInput;
+        }
+        else {
+            window.location="https://www.google.com/search?source=hp&ei=kMbjW4bEIYnRjwSJtKPIDQ&q=" + this.state.currentInput;
+        }
     }
     handleInputChange(e) {
-        this.setState({
-            currentInput: e.target.value
-        })
+        let results = search.autocompleteSearch(e.target.value.toLowerCase());
+        if (results.length == 2 && results[1] == true) {
+            this.setState({
+                isLink: true,
+                currentInput: e.target.value
+            })
+        }
+        else {
+            this.setState({
+                currentInput: e.target.value,
+                autocompleteOptions: results,
+                isLink: false
+            })
+        }
     }
 }
 
