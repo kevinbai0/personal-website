4a127c23c5c692d934e3c588e214a689a269d52a
kevinbai0 <kevin.c.bai0@gmail.com>
2018-11-08 00:29:02 -0500

    Added google search into searchEngine

--- client/src/components/pages/SearchEngine.js
+++ client/src/components/pages/SearchEngine.js
@@ -4,8 +4,10 @@ class SearchEngine extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            imageURL: ""
+            imageURL: "",
+            currentInput: ""
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     componentDidMount() {
         this.getImage().then((json) => {
@@ -22,12 +24,26 @@ class SearchEngine extends Component {
             }
             <div className="app-content">   
                 <div className="welcome-title">Kevin Bai</div>
-                <input type="text" className="search-bar" id="mainSearchBar" placeholder="Search" autoFocus/>
+                <form onSubmit={this.handleSubmit} autoComplete="off">
+                    <input type="text" className="search-bar" id="mainSearchBar" placeholder="Search" onChange={(e) => this.handleInputChange(e)} autoFocus/>
+                    <input type="submit" value="submit" className="hidden"/>
+                </form>
+                
             </div>
         </div>
     }
     getImage() {
         return fetch("/api/photoOfTheDay").then((res) => res.json());
+    }
+    handleSubmit(e) {
+        console.log("Handle submit");
+        e.preventDefault();
+        window.location="https://www.google.com/search?source=hp&ei=kMbjW4bEIYnRjwSJtKPIDQ&q=" + this.state.currentInput;
+    }
+    handleInputChange(e) {
+        this.setState({
+            currentInput: e.target.value
+        })
     }
 }
 
