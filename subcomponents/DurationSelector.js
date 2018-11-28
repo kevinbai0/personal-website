c920009a6f5ede6a5ab150f5de3f5e26f39c4919
kevinbai0 <kevin.c.bai0@gmail.com>
2018-11-27 20:41:05 -0500

    Added stop-motion

---
+++ subcomponents/DurationSelector.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../stylesheets/durationSelectorStyle.css";
+import 'rc-slider/assets/index.css';
+import Slider from 'rc-slider';
+
+class DurationSelector extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 15,
+            duration: 15
+        }
+    }
+    render() {
+        return <div className="duration-selector">
+            <div className="duration-title">
+                Frames Per <br/> Second
+            </div>
+           <Slider 
+                min={1} 
+                max={30} 
+                value={this.state.value} 
+                defaultValue={this.state.value} 
+                onChange={(e) => this.changeValue(e)}
+            />
+            <div className="duration-input">{this.state.duration + " fps"}</div>
+        </div>
+    }
+
+    changeValue(e) {
+        this.props.durationSetter(1000 / e);
+        this.setState({
+            value: e,
+            duration: e
+        })
+    }
+    changeValueInput(e) {
+        let value = e.target.value;
+    }
+}
+
+export default DurationSelector;
