7b1b857b36a1cb5c78ad29ac1cf3a0b69e976f9f
kevinbai0 <kevin.c.bai0@gmail.com>
2018-12-25 15:06:24 -0500

    Added process env port

--- server.js
+++ server.js
@@ -56,9 +56,9 @@ app.prepare()
         });
     });
 
-    server.listen(3000, (err) => {
+    server.listen(process.env.PORT || 3000, (err) => {
         if (err) throw err
-        console.log('> Ready on http://localhost:3000')
+        console.log('> Ready on http://localhost:' + process.env.PORT || 3000)
     })
 })
 .catch((ex) => {
