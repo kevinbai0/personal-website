10900a63e321a18928aa51452cd6ff0cd182d93b
kevinbai0 <kevin.c.bai0@gmail.com>
2018-12-25 14:37:29 -0500

    Added nodemailer emailing functionality

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const next = require('next')
+const nodeMailer = require("nodemailer");
+const login = require("./secrets/email_auth");
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+    const server = express()
+    server.use(express.json());
+
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.get("/", (req, res) => {
+        app.render(req, res, "/");
+    })
+    server.post("/api/email", (req,res) => {
+        var transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.NODEMAILER_EMAIL || login.email,
+                pass: process.env.NODEMAILER_PASSWORD || login.password
+            }
+        });
+
+        const mailOptions = {
+            from: req.body.email, // sender address
+            to: login.email, // list of receivers
+            subject: "Client: " + req.body.subject, // Subject line
+            html: "<p>Email: " + req.body.email + "</p>" 
+                + "<p>Name: " + req.body.name + "</p>" 
+                + "<p>Subject: " + req.body.subject + "</p>"
+                + "<p>" + req.body.message.split("\n").join("<br/>") + "</p>"
+        };
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) res.status(500).send("error");
+            else res.send("ok");
+        });
+    });
+
+    server.listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
