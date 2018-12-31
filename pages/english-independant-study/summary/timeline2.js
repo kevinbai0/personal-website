1ca5558b5aeb5ff4333ff5f2e12fc3b790e46418
kevinbai0 <kevin.c.bai0@gmail.com>
2018-12-31 18:56:42 -0500

    Removed unnecessary pages in english independant study seminar

--- pages/english-independant-study/summary/timeline2.js
+++
@@ -1,3 +0,0 @@
-import SummaryTimelinePage from "../../components/pages/SummaryTimelinePage";
-
-export default () => <SummaryTimelinePage shouldSkipExpand nextRoute="/summary/sub-timeline2" customCondensed={[6,7]}/>
--- pages/english-independant-study/summary/timeline3.js
+++
@@ -1,3 +0,0 @@
-import SummaryTimelinePage from "../../components/pages/SummaryTimelinePage";
-
-export default () => <SummaryTimelinePage shouldSkipExpand nextRoute="/summary/sub-timeline3" customCondensed={[7]}/>
