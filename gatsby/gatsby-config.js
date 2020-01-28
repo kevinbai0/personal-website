27d65ecedf2c6eda091c5860a78d987b93ad1e0e
Kevin Bai <kevin.c.bai0@gmail.com>
2020-01-28 02:08:57 -0500

    Added favicon and updated manifest

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -23,13 +23,13 @@ module.exports = {
         {
             resolve: "gatsby-plugin-manifest",
             options: {
-                name: "gatsby-starter-default",
-                short_name: "starter",
+                name: "Kevin Bai",
+                short_name: "kevinbai",
                 start_url: "/",
-                background_color: "#663399",
-                theme_color: "#663399",
+                background_color: "#FAF7F5",
+                theme_color: "#D18917",
                 display: "minimal-ui",
-                icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+                icon: "src/images/favicon.png", // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
